08c2d0a7917ef8e325c5c0acb80b552f11d970ee
ravindravp <ravindravpauskar@gmail.com>
2025-05-24 23:14:43 +0530

    Update deployManager.groovy

--- vars/deployManager.groovy
+++ vars/deployManager.groovy
@@ -1,9 +1,9 @@
 def call() {
     def ec2Ip = '13.51.162.152'
-    def sshKeyPath = '/path/to/your/ssh-key.pem'
+    def sshKeyPath = 'C:/Users/2000149437/Downloads/ravindrap.pem'
     echo "Deploying application to EC2 instance: ${ec2Ip}"
     sh """
-        scp -i ${sshKeyPath} target/sample-java-app-1.0-SNAPSHOT.jar ec2-user@${ec2Ip}:/home/ec2-user/
-        ssh -i ${sshKeyPath} ec2-user@${ec2Ip} "java -jar /home/ec2-user/sample-java-app-1.0-SNAPSHOT.jar"
+        scp -i ${sshKeyPath} target/HEX-jenkins-1.0-SNAPSHOT.jar ec2-user@${ec2Ip}:/home/ec2-user/
+        ssh -i ${sshKeyPath} ec2-user@${ec2Ip} "java -jar /home/ec2-user/HEX-jenkins-1.0-SNAPSHOT.jar"
     """
 }
