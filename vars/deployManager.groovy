440b0ca35488ceb0fb2869a8d277be370127d941
ravindravp <ravindravpauskar@gmail.com>
2025-05-24 22:57:06 +0530

    Update deployManager.groovy

--- vars/deployManager.groovy
+++ vars/deployManager.groovy
@@ -1,3 +1,9 @@
 def call() {
-    sh 'echo Hi, Welcome to Hexaware'
+    def ec2Ip = 'EC2_PUBLIC_IP'
+    def sshKeyPath = '/path/to/your/ssh-key.pem'
+    echo "Deploying application to EC2 instance: ${ec2Ip}"
+    sh """
+        scp -i ${sshKeyPath} target/sample-java-app-1.0-SNAPSHOT.jar ec2-user@${ec2Ip}:/home/ec2-user/
+        ssh -i ${sshKeyPath} ec2-user@${ec2Ip} "java -jar /home/ec2-user/sample-java-app-1.0-SNAPSHOT.jar"
+    """
 }
