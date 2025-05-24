b31fb60d5722b7c62f20d40d3fa7f68b60dc0832
ravindravp <ravindravpauskar@gmail.com>
2025-05-24 23:56:04 +0530

    Update deployManager.groovy

--- vars/deployManager.groovy
+++ vars/deployManager.groovy
@@ -1,9 +1,14 @@
 def call() {
-    def ec2Ip = '13.51.162.152'
-    def sshKeyPath = '/c/Users/2000149437/Downloads/ravindrap.pem'
+    def ec2Ip = '13.51.162.152' // Replace with your EC2 instance's public IP
+    def sshKeyPath = '/c/Users/2000149437/Downloads/ravindrap.pem' // Path to your .pem file
+    def appJar = 'target/HEX-jenkins-1.0-SNAPSHOT.jar' // Correct application JAR file path
+
     echo "Deploying application to EC2 instance: ${ec2Ip}"
     sh """
-    scp -o StrictHostKeyChecking=no -i ${sshKeyPath} target/sample-java-app-1.0-SNAPSHOT.jar ec2-user@${ec2Ip}:/home/ec2-user/
-    ssh -o StrictHostKeyChecking=no -i ${sshKeyPath} ec2-user@${ec2Ip} "java -jar /home/ec2-user/sample-java-app-1.0-SNAPSHOT.jar"
-"""
+        # Copy the JAR file to the EC2 instance
+        scp -o StrictHostKeyChecking=no -i ${sshKeyPath} ${appJar} ec2-user@${ec2Ip}:/home/ec2-user/
+
+        # Run the application on the EC2 instance
+        ssh -o StrictHostKeyChecking=no -i ${sshKeyPath} ec2-user@${ec2Ip} "java -jar /home/ec2-user/HEX-jenkins-1.0-SNAPSHOT.jar"
+    """
 }
