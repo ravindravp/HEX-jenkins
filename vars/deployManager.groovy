acb97a85911853ebffd77c8d2ef3c442c058d479
ravindravp <ravindravpauskar@gmail.com>
2025-05-24 23:19:08 +0530

    Update deployManager.groovy

--- vars/deployManager.groovy
+++ vars/deployManager.groovy
@@ -3,7 +3,7 @@ def call() {
     def sshKeyPath = 'C:/Users/2000149437/Downloads/ravindrap.pem'
     echo "Deploying application to EC2 instance: ${ec2Ip}"
     sh """
-        scp -i ${sshKeyPath} target/HEX-jenkins-1.0-SNAPSHOT.jar ec2-user@${ec2Ip}:/home/ec2-user/
-        ssh -i ${sshKeyPath} ec2-user@${ec2Ip} "java -jar /home/ec2-user/HEX-jenkins-1.0-SNAPSHOT.jar"
+        scp -i ${sshKeyPath} target/sample-java-app-1.0-SNAPSHOT.jar ec2-user@${ec2Ip}:/home/ec2-user/
+        ssh -i ${sshKeyPath} ec2-user@${ec2Ip} "java -jar /home/ec2-user/sample-java-app-1.0-SNAPSHOT.jar"
     """
 }
