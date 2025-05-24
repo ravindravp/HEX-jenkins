d9c74bccc5cf8385c2b403408a151f4504ac52cc
ravindravp <ravindravpauskar@gmail.com>
2025-05-24 23:29:40 +0530

    Update deployManager.groovy

--- vars/deployManager.groovy
+++ vars/deployManager.groovy
@@ -1,6 +1,6 @@
 def call() {
     def ec2Ip = '13.51.162.152'
-    def sshKeyPath = '/mnt/c/Users/2000149437/Downloads/ravindrap.pem'
+    def sshKeyPath = '/c/Users/2000149437/Downloads/ravindrap.pem'
     echo "Deploying application to EC2 instance: ${ec2Ip}"
     sh """
         scp -i ${sshKeyPath} target/sample-java-app-1.0-SNAPSHOT.jar ec2-user@${ec2Ip}:/home/ec2-user/
