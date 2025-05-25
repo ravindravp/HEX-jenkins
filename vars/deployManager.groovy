26ed66da44778e5cb7069c15c742d9c8842749f9
ravindravp <ravindravpauskar@gmail.com>
2025-05-25 23:32:38 +0530

    Update deployManager.groovy

--- vars/deployManager.groovy
+++ vars/deployManager.groovy
@@ -1,9 +1,12 @@
 def call() {
-    def ec2Ip = '13.51.162.250'
-    def sshKeyPath = '/c/Users/2000149437/Downloads/ravindrap.pem' // Corrected path
+    def ec2Ip = '16.171.2.15'
     echo "Deploying application to EC2 instance: ${ec2Ip}"
-    sh """
-        scp -o StrictHostKeyChecking=no -i ${sshKeyPath} target/sample-java-app-1.0-SNAPSHOT.jar ubuntu@${ec2Ip}:/home/ubuntu/
-        ssh -o StrictHostKeyChecking=no -i ${sshKeyPath} ubuntu@${ec2Ip} "java -jar /home/ubuntu/sample-java-app-1.0-SNAPSHOT.jar"
-    """
+    
+    // Use the Secret File stored in Jenkins credentials
+    withCredentials([file(credentialsId: 'your-credential-id', variable: 'SSH_KEY_PATH')]) {
+        sh """
+            scp -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} target/sample-java-app-1.0-SNAPSHOT.jar ubuntu@${ec2Ip}:/home/ubuntu/
+            ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ubuntu@${ec2Ip} "java -jar /home/ubuntu/sample-java-app-1.0-SNAPSHOT.jar"
+        """
+    }
 }
